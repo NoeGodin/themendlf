24a4050ab3e79ccd9a6b75530b62ea3d235233e1
Noé GODIN <118174685+NoeGodin@users.noreply.github.com>
2023-12-08 07:53:44 +0100

    Delete ndli-front-main directory

--- ndli-front-main/pageTheme/sliderAnimation.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("showSlidersButton").addEventListener("click", toggleSlidersVisibility);
-
-function toggleSlidersVisibility() {
-    const slidersContainer = document.getElementById("slidersContainer");
-    slidersContainer.classList.toggle("visible");
-    slidersContainer.classList.toggle("hidden");
-}
--- ndli-front-main/scripts/scripts.js
+++
@@ -1,5 +0,0 @@
-import connexion from "./api/connexion";
-import inscription from "./api/inscription";
-import testapi from "./api/testapi";
-
-export { connexion, inscription, testapi };
